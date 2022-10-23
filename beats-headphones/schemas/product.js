d1ebe6b2d54f5076a8f3552f06573de4bbd47118
Tyrone Woody <bobbywoody7@gmail.com>
2022-10-22 21:37:59 -0600

    schema

--- beats-headphones/schemas/product.js
+++ beats-headphones/schemas/product.js
@@ -1,85 +1,40 @@
 export default {
-  name: 'product',
-  title: 'Product',
-  type: 'document',
+  name: "product",
+  title: "Product",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "title",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
       options: {
-        source: 'title',
-        maxLength: 96,
+        hotspot: true,
       },
     },
     {
-      title: 'Default variant',
-      name: 'defaultProductVariant',
-      type: 'productVariant',
+      name: "name",
+      title: "Name",
+      type: "string",
     },
     {
-      title: 'Variants',
-      name: 'variants',
-      type: 'array',
-      of: [
-        {
-          title: 'Variant',
-          type: 'productVariant',
-        },
-      ],
-    },
-    {
-      title: 'Tags',
-      name: 'tags',
-      type: 'array',
-      of: [
-        {
-          type: 'string',
-        },
-      ],
-      options: {
-        layout: 'tags',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      option: {
+        source: "name",
+        maxLength: 90,
       },
     },
     {
-      name: 'vendor',
-      title: 'Vendor',
-      type: 'reference',
-      to: {type: 'vendor'},
+      name: "price",
+      title: "Price",
+      type: "number",
     },
     {
-      name: 'blurb',
-      title: 'Blurb',
-      type: 'localeString',
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: {type: 'category'},
-        },
-      ],
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'localeBlockContent',
+      name: "details",
+      title: "Details",
+      type: "string",
     },
   ],
-
-  preview: {
-    select: {
-      title: 'title',
-      manufactor: 'manufactor.title',
-      media: 'defaultProductVariant.images[0]',
-    },
-  },
-}
+};
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Bobby</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
-console.log();
 export default App;
