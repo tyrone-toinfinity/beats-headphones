2205fd4f1444b1983597e10ede72129355ddd148
Tyrone Woody <bobbywoody7@gmail.com>
2022-10-22 19:19:16 -0600

    first push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bobby</h1>
+    </div>
+  );
+}
+console.log();
+export default App;
