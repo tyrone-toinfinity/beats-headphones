5789b649e6e52a9c27f7ad6b8a626aa3fb14014e
Tyrone Woody <bobbywoody7@gmail.com>
2022-11-24 20:39:29 -0700

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Navbar.css";
-import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaSearch, FaShoppingCart, FaBars } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
+import { VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 export const Navbar = ({ navRef }) => {
+  const navbarRef = useRef();
+  const showNavbar = () => {
+    navbarRef.current.classList.toggle();
+  };
+
   return (
     <nav>
       <div className="desktop-nav" ref={navRef}>
@@ -32,27 +39,38 @@ export const Navbar = ({ navRef }) => {
           <p>{0}</p>
         </div>
       </div>
-
-      <div className="mobile-nav">
-        <div>
-          <Link to="">
-            <img src="./beats-logo.svg" alt="logo" className="logo" />
-          </Link>
+      <div className="mobileWrapper">
+        <div className="mobile-nav">
+          <FaBars className="mobileNavHamburger" onClick={showNavbar} />
+          <div>
+            <Link to="/">
+              <img src="./beats-logo.svg" alt="logo" className="logo" />
+            </Link>
+          </div>
+          <div>
+            <div className="shoppingCart">
+              <Link to="/cart" className="cartLogo">
+                <FiShoppingCart />
+              </Link>
+              <p>{0}</p>
+            </div>
+          </div>
         </div>
-        <ul className="navbar">
-          <Link to="">
+        <ul className="navbar__Mobile">
+          <button>
+            {" "}
+            <VscClose className="mobileNavClose" />
+          </button>
+          <Link to="/">
             <li>discover</li>
           </Link>
-          <Link to="">
+          <Link to="/shop">
             <li>shop</li>
           </Link>
-          <Link to="">
+          <Link to="/support">
             <li>support</li>
           </Link>
         </ul>
-        <div>
-          <FaSearch />
-        </div>
       </div>
     </nav>
   );
