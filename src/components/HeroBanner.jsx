60bfda8043ff35e653247b5ef7f6a3c2964b6368
Tyrone Woody <bobbywoody7@gmail.com>
2023-01-22 15:30:12 -0700

    autoplay mobile

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,14 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import "./HeroBanner.css";
+import { useEffect, useRef } from "react";
 
 export const HeroBanner = () => {
   const navigate = useNavigate();
+
+  const isMobile = /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
+
   return (
     <main className="hero-banner-container">
       <div className="banner-wrapper">
-        <video playsinline loop muted autoPlay className="heroVideo">
+        <video
+          muted={isMobile ? false : true}
+          playsinline={!isMobile}
+          loop
+          autoPlay
+          className="heroVideo"
+        >
           <source src="./assets/videos/beats-video.mp4" type="video/mp4" />
         </video>
+
         <div className="containerLandingText">
           <h1>LEBRON & BRONNY</h1>
           <p>Powered by Beats Fit Pro</p>
